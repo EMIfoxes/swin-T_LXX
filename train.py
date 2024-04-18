bbcffe33a8c51245a4ecf8f9e37db18555c57763
sec-lxx <990679311@qq.com>
2024-04-18 22:13:43 +0800

    1

---
+++ train.py
@@ -0,0 +1,165 @@
+import os
+import argparse
+
+import torch
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+
+from my_dataset import MyDataSet
+from model import swin_tiny_patch4_window7_224 as create_model
+from utils import read_split_data, train_one_epoch, evaluate
+
+import subprocess
+import datetime
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    
+    result =subprocess.check_output(['git','rev-parse','--abbrev-ref','HEAD'])
+    branch_name =result.decode('utf-8').strip()
+
+    now = datetime.datetime.now()  
+    formatted_datetime = now.strftime("%Y-%m-%d_%H_%M")  
+    
+    dir_str = os.path.join(branch_name,formatted_datetime)
+
+    if os.path.exists("./weights/{}/".format(dir_str)) is False:
+        os.makedirs("./weights/{}/".format(dir_str))
+
+    tb_writer = SummaryWriter()
+
+    train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(args.data_path)
+
+    img_size = 224
+    data_transform = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(img_size),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        "val": transforms.Compose([transforms.Resize(int(img_size * 1.143)),
+                                   transforms.CenterCrop(img_size),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
+
+    # 实例化训练数据集
+    train_dataset = MyDataSet(images_path=train_images_path,
+                              images_class=train_images_label,
+                              transform=data_transform["train"])
+
+    # 实例化验证数据集
+    val_dataset = MyDataSet(images_path=val_images_path,
+                            images_class=val_images_label,
+                            transform=data_transform["val"])
+
+    batch_size = args.batch_size
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               num_workers=nw,
+                                               collate_fn=train_dataset.collate_fn)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=batch_size,
+                                             shuffle=False,
+                                             pin_memory=True,
+                                             num_workers=nw,
+                                             collate_fn=val_dataset.collate_fn)
+
+    model = create_model(num_classes=args.num_classes).to(device)
+
+    if args.weights != "":
+        assert os.path.exists(args.weights), "weights file: '{}' not exist.".format(args.weights)
+        weights_dict = torch.load(args.weights, map_location=device)["model"]
+        # 删除有关分类类别的权重
+        for k in list(weights_dict.keys()):
+            if "head" in k:
+                del weights_dict[k]
+        print(model.load_state_dict(weights_dict, strict=False))
+
+    if args.freeze_layers:
+        for name, para in model.named_parameters():
+            # 除head外，其他权重全部冻结
+            if "head" not in name:
+                para.requires_grad_(False)
+            else:
+                print("training {}".format(name))
+
+    pg = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.AdamW(pg, lr=args.lr, weight_decay=5E-2)
+
+    # 将模型写入tensorboard
+    init_img = torch.zeros((1, 3, 224, 224), device=device)
+    tb_writer.add_graph(model, init_img)
+
+    best_dir = 'nothing'
+    last_dir = 'nothing'
+    for epoch in range(args.epochs):
+        # train
+        train_loss, train_acc = train_one_epoch(model=model,
+                                                optimizer=optimizer,
+                                                data_loader=train_loader,
+                                                device=device,
+                                                epoch=epoch)
+
+        # validate
+        val_loss, val_acc = evaluate(model=model,
+                                     data_loader=val_loader,
+                                     device=device,
+                                     epoch=epoch)
+
+        tags = ["train_loss", "train_acc", "val_loss", "val_acc", "learning_rate"]
+        tb_writer.add_scalar(tags[0], train_loss, epoch)
+        tb_writer.add_scalar(tags[1], train_acc, epoch)
+        tb_writer.add_scalar(tags[2], val_loss, epoch)
+        tb_writer.add_scalar(tags[3], val_acc, epoch)
+        tb_writer.add_scalar(tags[4], optimizer.param_groups[0]["lr"], epoch)
+        
+        #保存最好一次的权重
+        if val_loss < args.min_loss_to_save:
+            
+            #删除上一次的model-best.pth
+            if os.path.isfile(best_dir):
+                os.remove(best_dir)
+            
+            args.min_loss_to_save = val_loss
+            best_dir = "./weights/{}/model-best_{}.pth".format(dir_str,epoch)
+            torch.save(model.state_dict(), best_dir)
+        
+        
+        #删除上一次的model-last.pth
+        if os.path.isfile(last_dir):
+            os.remove(last_dir)
+
+        #保存最后一次的权重
+        last_dir =  "./weights/{}/model-last_{}.pth".format(dir_str,epoch)
+        torch.save(model.state_dict(), last_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_classes', type=int, default=5)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch-size', type=int, default=8)
+    parser.add_argument('--lr', type=float, default=0.0001)
+
+    # 数据集所在根目录
+    # https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz
+    parser.add_argument('--data-path', type=str,
+                        default="D:/DL_Data/flower_photos")
+
+    # 预训练权重路径，如果不想载入就设置为空字符
+    parser.add_argument('--weights', type=str, default='', #swin_tiny_patch4_window7_224.pth
+                        help='initial weights path')
+    # 是否冻结权重
+    parser.add_argument('--freeze-layers', type=bool, default=False)
+    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+    # 当损失值小于n时，开始保存最佳模型权重
+    parser.add_argument('--min_loss_to_save',type=float,default=2)
+
+    opt = parser.parse_args()
+
+    main(opt)
