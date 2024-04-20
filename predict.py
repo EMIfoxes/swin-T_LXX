e3e60c0e9ec3453d3e7f4f047bee087a59eed892
sec-lxx <990679311@qq.com>
2024-04-20 21:09:29 +0800

    feat(predict.py)

--- predict.py
+++ predict.py
@@ -6,29 +6,21 @@ from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
 
-from model import swin_tiny_patch4_window7_224 as create_model
-
+from model import swin_tiny_patch4_window7_224 as create_model,seq_SwinTransformer
+import hdf5storage
 
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    img_size = 224
-    data_transform = transforms.Compose(
-        [transforms.Resize(int(img_size * 1.14)),
-         transforms.CenterCrop(img_size),
-         transforms.ToTensor(),
-         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
     # load image
-    img_path = "../tulip.jpg"
+    img_path = "D:/LXX/Data/UVA/MF3/MF3_91122.mat"
     assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
-    img = Image.open(img_path)
-    plt.imshow(img)
-    # [N, C, H, W]
-    img = data_transform(img)
-    # expand batch dimension
-    img = torch.unsqueeze(img, dim=0)
 
+    data = hdf5storage.loadmat(img_path)['single_data']
+    data = torch.tensor(data,dtype=torch.float32).reshape(125,128).unsqueeze(0)
+    data = torch.unsqueeze(data, dim=0)
+   
     # read class_indict
     json_path = './class_indices.json'
     assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
@@ -37,14 +29,22 @@ def main():
         class_indict = json.load(f)
 
     # create model
-    model = create_model(num_classes=5).to(device)
+    model = seq_SwinTransformer(in_chans=3,
+                                patch_size=4,
+                                window_size=7,
+                                embed_dim=96,
+                                depths=(2, 2, 6, 2),
+                                num_heads=(3, 6, 12, 24),
+                                num_classes=9,
+                                ).to(device)
+
     # load model weights
-    model_weight_path = "./weights/model-9.pth"
+    model_weight_path = "weights/seq_predict/2024-04-20_20_46/model-best_7.pth"
     model.load_state_dict(torch.load(model_weight_path, map_location=device))
     model.eval()
     with torch.no_grad():
         # predict class
-        output = torch.squeeze(model(img.to(device))).cpu()
+        output = torch.squeeze(model(data.to(device))).cpu()
         predict = torch.softmax(output, dim=0)
         predict_cla = torch.argmax(predict).numpy()
 
